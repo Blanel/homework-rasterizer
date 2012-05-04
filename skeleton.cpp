6a1c57aafe5f7d8a3f60ad14c3b9c9b7d58edc97
Mohed <Mohed@hotmail.com>
2012-05-04 15:47:35 +0200

    bla
    bla

--- skeleton.cpp
+++ skeleton.cpp
@@ -12,7 +12,6 @@ using glm::mat3;
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 
-
 struct Pixel
 {
     int x;
@@ -20,22 +19,28 @@ struct Pixel
     float zinv;
 };
 
-
-
+// Screen
 const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
+
+// Ticker
 int t;
+
+// World
 vector<Triangle> triangles;
+
+// Camera
 int f = 250;
 vec3 camPosition(0,0,-2);
+
+// Misc
 vec3 color;
-vector<Pixel> leftPixels;
+vector<Pixel> leftPixels();
 vector<Pixel> rightPixels;
 
 // ----------------------------------------------------------------------------
 // FUNCTIONS
-
 
 void Update();
 void Draw();
@@ -118,25 +123,38 @@ void Draw()
 	if( SDL_MUSTLOCK(screen) )
 		SDL_LockSurface(screen);
 	
-	//for( int i=0; i<triangles.size(); i++ )
-	//{
+	for( int i=0; i<triangles.size(); ++i )
+	{
 		vector<vec3> vertices(3);
         
-		vertices[0] = triangles[4].v0;
-		vertices[1] = triangles[4].v1;
-		vertices[2] = triangles[4].v2;
-        color = triangles[4].color;
+		vertices[0] = triangles[i].v0;
+		vertices[1] = triangles[i].v1;
+		vertices[2] = triangles[i].v2;
+        color = triangles[i].color;
         
 		// Add drawing
         
         DrawPolygon( vertices );
-        vertices.clear();
-    //}
+        // vertices.clear();
+    }
 	
 	if ( SDL_MUSTLOCK(screen) )
 		SDL_UnlockSurface(screen);
     
 	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+}
+void DrawPolygon( const vector<vec3>& vertices )
+{
+    int V = vertices.size();
+    vector<Pixel> vertexPixels( V );
+    for( int i=0; i<V; ++i )
+        VertexShader( vertices[i], vertexPixels[i] );
+    
+    ComputePolygonRows( vertexPixels, leftPixels, rightPixels );
+    DrawRows( leftPixels, rightPixels );
+    
+    leftPixels.clear();
+    rightPixels.clear();
 }
 void VertexShader( const vec3& v, Pixel& p )
 {
@@ -144,6 +162,72 @@ void VertexShader( const vec3& v, Pixel& p )
     p.x = f*(vLocal.x/(vLocal.z))+SCREEN_WIDTH/2;
     p.y = f*(vLocal.y/(vLocal.z))+SCREEN_HEIGHT/2;
     p.zinv = 1/vLocal.z;
+}
+
+void ComputePolygonRows(
+                        const vector<Pixel>& vertexPixels,
+                        vector<Pixel>& leftPixels,
+                        vector<Pixel>& rightPixels )
+{
+    // 1. Find max and min y-value of the polygon
+    // and compute the number of rows it occupies.
+    
+    int V = vertexPixels.size();
+    int max = -1;
+    int min = 501;
+    int ROWS;
+    
+    for (int i = 0; i < V; ++i)
+    {
+        max = vertexPixels[i].y > max ? vertexPixels[i].y : max;
+        min = vertexPixels[i].y < min ? vertexPixels[i].y : min;
+        ROWS = max-min+1;
+    }
+    
+    
+    // 2. Resize leftPixels and rightPixels
+    // so that they have an element for each row.
+    
+    leftPixels.clear();
+    rightPixels.clear();
+    leftPixels.resize(ROWS);
+    rightPixels.resize(ROWS);
+    
+    
+    // 3. Initialize the x-coordinates in leftPixels
+    // to some really large value and the x-coordinates
+    // in rightPixels to some really small value.
+    
+    for( int i=0; i < ROWS; ++i )
+    {
+        leftPixels[i].x = +numeric_limits<int>::max();
+        rightPixels[i].x = -numeric_limits<int>::max();
+    }
+    
+    // 4. Loop through all edges of the polygon and use
+    // linear interpolation to find the x-coordinate for
+    // each row it occupies. Update the corresponding
+    // values in rightPixels and leftPixels.
+    
+    // Loop over all vertices and compute the edge from it to the next vertex:
+    
+    vector<Pixel> result(V*ROWS);
+    for( int i=0; i<V; ++i )
+    {
+        int j = (i+1)%V;                    // The next vertex
+        vector<Pixel> resultVertex(ROWS);
+        Interpolate(vertexPixels[i], vertexPixels[j], resultVertex);
+        result.insert(result.end(), resultVertex.begin(), resultVertex.end());
+    }
+    
+    int row;
+    for (int i = 0; i < result.size(); i++) {
+        row = result[i].y-min;
+        leftPixels[row].y = result[i].y;
+        leftPixels[row].x = result[i].x < leftPixels[row].x ? result[i].x : leftPixels[row].x;
+        rightPixels[row].y = result[i].y;
+        rightPixels[row].x = result[i].x > rightPixels[row].x ? result[i].x : rightPixels[row].x;
+    }
 }
 void Interpolate( Pixel a, Pixel b, vector<Pixel>& result )
 {
@@ -168,69 +252,7 @@ void Interpolate( Pixel a, Pixel b, vector<Pixel>& result )
         //<< rightPixels[row].y << "). " 
     }
 }
-void ComputePolygonRows(
-                        const vector<Pixel>& vertexPixels,
-                        vector<Pixel>& leftPixels,
-                        vector<Pixel>& rightPixels )
-{
-    // 1. Find max and min y-value of the polygon
-    // and compute the number of rows it occupies.
-    
-    int V = vertexPixels.size();
-    int max = -1;
-    int min = 501;
-    int ROWS;
-    
-    for (int i = 0; i < V; i++)
-    {
-        max = vertexPixels[i].y > max ? vertexPixels[i].y : max;
-        min = vertexPixels[i].y < min ? vertexPixels[i].y : min;
-        ROWS = max-min+1;
-    }
-    
-    
-    // 2. Resize leftPixels and rightPixels
-    // so that they have an element for each row.
-    
-    leftPixels.resize(ROWS);
-    rightPixels.resize(ROWS);
-    
-    
-    // 3. Initialize the x-coordinates in leftPixels
-    // to some really large value and the x-coordinates
-    // in rightPixels to some really small value.
-    
-    for( int i=0; i < ROWS; i++ )
-    {
-        leftPixels[i].x = +numeric_limits<int>::max();
-        rightPixels[i].x = -numeric_limits<int>::max();
-    }
-    
-    // 4. Loop through all edges of the polygon and use
-    // linear interpolation to find the x-coordinate for
-    // each row it occupies. Update the corresponding
-    // values in rightPixels and leftPixels.
-    
-    // Loop over all vertices and compute the edge from it to the next vertex:
-    
-    vector<Pixel> result;
-    for( int i=0; i<V; i++ )
-    {
-        int j = (i+1)%V;                    // The next vertex
-        vector<Pixel> resultVertex(ROWS);
-        Interpolate(vertexPixels[i], vertexPixels[j], resultVertex);
-        result.insert(result.end(), resultVertex.begin(), resultVertex.end());
-    }
-    
-    int row;
-    for (int i = 0; i < result.size(); i++) {
-        row = result[i].y-min;
-        leftPixels[row].y = result[i].y;
-        leftPixels[row].x = result[i].x < leftPixels[row].x ? result[i].x : leftPixels[row].x;
-        rightPixels[row].y = result[i].y;
-        rightPixels[row].x = result[i].x > rightPixels[row].x ? result[i].x : rightPixels[row].x;
-    }
-} 
+
 void DrawRows(
               const vector<Pixel>& leftPixels,
               const vector<Pixel>& rightPixels )
@@ -243,17 +265,5 @@ void DrawRows(
         }
     }
 }
-void DrawPolygon( const vector<vec3>& vertices )
-{
-    int V = vertices.size();
-    vector<Pixel> vertexPixels( V );
-    for( int j=0; j<V; j++ )
-        VertexShader( vertices[j], vertexPixels[j] );
-    
-    ComputePolygonRows( vertexPixels, leftPixels, rightPixels );
-    DrawRows( leftPixels, rightPixels );
-        
-    leftPixels.clear();
-    rightPixels.clear();
-}
+
 
