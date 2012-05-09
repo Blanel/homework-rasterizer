6e8ca6c9c64cd8bafb3145bb8c3e0fdd308646fe
Eric Nellåker <eric.nellaker@gmail.com>
2012-05-09 11:44:11 +0200

    Fixed rotation. Segfault when inside box

--- skeleton.cpp
+++ skeleton.cpp
@@ -55,6 +55,7 @@ float thetaX = 0;
 float thetaY = 0;
 float thetaZ = 0;
 
+bool DEBUG=false;
 // ----------------------------------------------------------------------------
 // FUNCTIONS
 
@@ -77,100 +78,123 @@ vec3 Light( const Pixel& i );
 
 int main( int argc, char* argv[] )
 {
-	LoadTestModel( triangles );
+        LoadTestModel( triangles );
     Rotate();
-	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
-	t = SDL_GetTicks();	// Set start value for timer.
-    
-	while( NoQuitMessageSDL() )
-	{
-		Update();
-		Draw();
+        screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
+        t = SDL_GetTicks();	// Set start value for timer.
+
+        while( NoQuitMessageSDL() )
+        {
+                Update();
+                Draw();
     }
-    
-	SDL_SaveBMP( screen, "screenshot.bmp" );
-	return 0;
+
+        SDL_SaveBMP( screen, "screenshot.bmp" );
+        return 0;
 }
 void Update()
 {
-	// Compute frame time:
-	int t2 = SDL_GetTicks();
-	float dt = float(t2-t);
-	t = t2;
-	cout << "Render time: " << dt << " ms." << endl;
-    
-	Uint8* keystate = SDL_GetKeyState(0);
-    
-	if( keystate[SDLK_UP] )
+        // Compute frame time:
+        int t2 = SDL_GetTicks();
+        float dt = float(t2-t);
+        t = t2;
+        cout << "Render time: " << dt << " ms." << endl;
+
+        Uint8* keystate = SDL_GetKeyState(0);
+
+        if( keystate[SDLK_y] )
         {
-            //thetaX -= 0.01;
-            camPosition.z += 0.05;
-            
+            Rotate();
+            camPosition += vec3(0,0,0.01)*rot;
+            //DEBUG=true;
+
         }
-    
-	if( keystate[SDLK_DOWN] )
+
+        if( keystate[SDLK_h] )
         {
-            //thetaX += 0.01;
-            camPosition.z -= 0.05;
+            Rotate();
+            camPosition -= vec3(0,0,0.01)*rot;
         }
-    
-	if( keystate[SDLK_RIGHT] )
+
+        if( keystate[SDLK_j] )
         {
-            thetaY -= 0.01;
-            camPosition.x += 0.05;
+            Rotate();
+            camPosition += vec3(0.01,0,0)*rot;
         }
-    
-	if( keystate[SDLK_LEFT] )
+
+        if( keystate[SDLK_g] )
         {
-            //thetaY += 0.01;
-            camPosition.x -= 0.05;
+            Rotate();
+            camPosition -= vec3(0.01,0,0)*rot;
         }
-    
-	if( keystate[SDLK_RSHIFT] )
-		;
-    
-	if( keystate[SDLK_RCTRL] )
-		;
-    
-	/*if( keystate[SDLK_w] )
+        if( keystate[SDLK_UP] )
+        {
+            thetaX-=0.01;
+            //DEBUG=true;
+
+        }
+
+        if( keystate[SDLK_DOWN] )
+        {
+            thetaX+=0.01;
+        }
+
+        if( keystate[SDLK_RIGHT] )
+        {
+            thetaY-=0.01;
+        }
+
+        if( keystate[SDLK_LEFT] )
+        {
+            thetaY+=0.01;
+        }
+
+        if( keystate[SDLK_RSHIFT] )
+                ;
+
+        if( keystate[SDLK_RCTRL] )
+                ;
+
+        /*if( keystate[SDLK_w] )
         {
             camPosition += rot*vec3(0,0,0.01);
         }
-    
-	if( keystate[SDLK_s] )
+
+        if( keystate[SDLK_s] )
         {
             camPosition += rot*vec3(0,0,0.01);
         }
-    
-	if( keystate[SDLK_d] )
+
+        if( keystate[SDLK_d] )
         {
             camPosition += rot*vec3(0.01,0,0);
         }
-    
-	if( keystate[SDLK_a] )
+
+        if( keystate[SDLK_a] )
         {
             camPosition -= rot*vec3(0.01,0,0);
         } */
     // Light movement
-    
+
     if( keystate[SDLK_w] )
-		lightPos.z += 0.1; 
-    
-	if( keystate[SDLK_s] )
-		lightPos.z -= 0.1;
-    
-	if( keystate[SDLK_a] )
-		lightPos.x -= 0.1;
-	
-	if( keystate[SDLK_d] )
-		lightPos.x += 0.1;
-    
-	if( keystate[SDLK_e] )
-		;
-    
-	if( keystate[SDLK_q] )
-		;        
-    
+                lightPos.z += 0.1;
+
+        if( keystate[SDLK_s] )
+                lightPos.z -= 0.1;
+
+        if( keystate[SDLK_a] )
+                lightPos.x -= 0.1;
+
+        if( keystate[SDLK_d] )
+                lightPos.x += 0.1;
+
+        if( keystate[SDLK_e] )
+                ;
+
+        if( keystate[SDLK_q] )
+                ;
+
+        cout<<"\ncam: "<<camPosition.x<<" "<<camPosition.y<<" "<<camPosition.z<<" "<<thetaX<<" "<<thetaY<<" "<<thetaZ;
     Rotate();
 
 }
@@ -188,59 +212,59 @@ void Rotate()
 }
 void Draw()
 {
-	SDL_FillRect( screen, 0, 0 );
-    
-	if( SDL_MUSTLOCK(screen) )
-		SDL_LockSurface(screen);
-	
-	// Clear the depthBuffer
+        SDL_FillRect( screen, 0, 0 );
+
+        if( SDL_MUSTLOCK(screen) )
+                SDL_LockSurface(screen);
+
+        // Clear the depthBuffer
     for( int y=0; y<SCREEN_HEIGHT; ++y )
         for( int x=0; x<SCREEN_WIDTH; ++x )
             depthBuffer[y][x] = 0;
-    
+
     for( int i=0; i<triangles.size(); ++i )
-	{
-		vector<Vertex> vertices(3);
+        {
+                vector<Vertex> vertices(3);
 
         vertices[0].position = triangles[i].v0;
         vertices[1].position = triangles[i].v1;
         vertices[2].position = triangles[i].v2;
 
-        
+
         // vertices[j].reflectance = 1;
 
         color = triangles[i].color;
         currentNormal = triangles[i].normal;
-        
-		// Add drawing
+
+                // Add drawing
         DrawPolygon( vertices );
 
     }
-	
-	if ( SDL_MUSTLOCK(screen) )
-		SDL_UnlockSurface(screen);
-    
-	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+
+        if ( SDL_MUSTLOCK(screen) )
+                SDL_UnlockSurface(screen);
+
+        SDL_UpdateRect( screen, 0, 0, 0, 0 );
 }
 void VertexShader( const Vertex& v, Pixel& p )
 {
     vec3 vLocal = v.position-camPosition;
-    
-    // vLocal = rot * vLocal;
-    
+
+    vLocal = rot*vLocal;
+
     p.zinv = 1/vLocal.z;
     p.x = (f * vLocal.x * p.zinv)+SCREEN_WIDTH/2;
     p.y = (f * vLocal.y * p.zinv)+SCREEN_HEIGHT/2;
     p.pos3d = v.position;
-    
-    
+
+
 }
 void Interpolate( Pixel a, Pixel b, vector<Pixel>& result )
 {
     int N = result.size();
     vec3 diff = vec3(b.x-a.x,b.y-a.y,b.zinv-a.zinv) / float(max(N-1,1));
     vec3 diffPos = vec3(b.pos3d*b.zinv - a.pos3d*a.zinv) / float(max(N-1,1));
-    
+
     vec3 current( a.x, a.y, a.zinv);
     vec3 currentPos(a.pos3d*a.zinv);
     for( int i=0; i<N; ++i )
@@ -249,7 +273,7 @@ void Interpolate( Pixel a, Pixel b, vector<Pixel>& result )
         result[i].y = current.y;
         result[i].zinv = current.z;
         result[i].pos3d = currentPos/current.z;
-                
+
         current.x += diff.x;
         current.y += diff.y;
         current.z += diff.z;
@@ -263,39 +287,39 @@ void ComputePolygonRows(
 {
     // 1. Find max and min y-value of the polygon
     // and compute the number of rows it occupies.
-    
+
     int V = vertexPixels.size();
     int max = -numeric_limits<int>::max();
     int min = +numeric_limits<int>::max();
-    
+
     for (int i = 0; i < V; ++i)
     {
         max = vertexPixels[i].y > max ? vertexPixels[i].y : max;
         min = vertexPixels[i].y < min ? vertexPixels[i].y : min;
     }
     int ROWS = max-min+1;
-    
+
     // 2. Resize leftPixels and rightPixels
     // so that they have an element for each row.
-    
-    leftPixels.resize(ROWS);    
+
+    leftPixels.resize(ROWS);
     rightPixels.resize(ROWS);
-    
+
     // 3. Initialize the x-coordinates in leftPixels
     // to some really large value and the x-coordinates
     // in rightPixels to some really small value.
-    
+
     for( int i=0; i < ROWS; ++i )
     {
         leftPixels[i].x = +numeric_limits<int>::max();
         rightPixels[i].x = -numeric_limits<int>::max();
     }
-    
+
     // 4. Loop through all edges of the polygon and use
     // linear interpolation to find the x-coordinate for
     // each row it occupies. Update the corresponding
     // values in rightPixels and leftPixels.
-    
+
     for( int i=0; i<V; ++i )
     {
         int j = (i+1)%V;                    // The next vertex
@@ -305,15 +329,15 @@ void ComputePolygonRows(
         for (int i = 0; i < result.size(); ++i){
             int row = result[i].y-min;
             leftPixels[row].y = result[i].y;
-            
+
             if (result[i].x < leftPixels[row].x){
                 leftPixels[row].x = result[i].x;
                 leftPixels[row].zinv = result[i].zinv;
                 leftPixels[row].pos3d = result[i].pos3d;
-                
+
             }
             rightPixels[row].y = result[i].y;
-            
+
             if (result[i].x > rightPixels[row].x){
                 rightPixels[row].x = result[i].x;
                 rightPixels[row].zinv = result[i].zinv;
@@ -342,10 +366,10 @@ void DrawPolygon( const vector<Vertex>& vertices )
     vector<Pixel> vertexPixels( V );
     for( int i=0; i<V; ++i )
         VertexShader( vertices[i], vertexPixels[i] );
-    
+
     vector<Pixel> leftPixels;
     vector<Pixel> rightPixels;
-    
+
     ComputePolygonRows( vertexPixels, leftPixels, rightPixels );
     DrawRows( leftPixels, rightPixels );
 }
@@ -353,6 +377,13 @@ void PixelShader( const Pixel& p )
 {
     int x = p.x;
     int y = p.y;
+    if(DEBUG)
+    {
+    cout<<"\nx: "<<x;
+    cout<<"y: "<<y;
+    cout<<"zinv: "<<p.zinv;
+    cout<<"dephthBuffer: "<<depthBuffer[y][x];
+    }
     if( p.zinv > depthBuffer[y][x] && x < SCREEN_WIDTH && x > 0 && y < SCREEN_HEIGHT && y > 0 )
     {
         depthBuffer[y][x] = p.zinv;
@@ -360,18 +391,18 @@ void PixelShader( const Pixel& p )
     }
 }
 vec3 Light( const Pixel& i ){
-    
+
     vec3 r = lightPos - i.pos3d ;
     vec3 rHat = glm::normalize(r);
-    
+
     float rLength = glm::length(r);
-    
+
     float rRatio = glm::dot(rHat,currentNormal);
     float ratio = rRatio >= 0 ? rRatio : 0;
-    
+
     float A = (4*3.14*rLength*rLength);
     vec3 B = lightPower/A;
     vec3 D = B*ratio;
-    
+
     return D+indirectLightPowerPerArea;
 }
